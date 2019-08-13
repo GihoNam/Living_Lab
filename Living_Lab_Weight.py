1a92d266197f4e8d34b7d00c76c77b991d2d92d4
GihoNam <41906145+GihoNam@users.noreply.github.com>
2019-08-13 14:23:11 +0900

    Update Weight

--- Living_Lab_Weight.py
+++ Living_Lab_Weight.py
@@ -1,6 +1,7 @@
 def Weight(Rssi):
     Rssi_result = []
     Rssi_Weight = [1/7,1/7,1/7,1/7,3/7]
-    for i in range(len(Rssi)):
-        Rssi_result.append(Rssi[i] * Rssi_Weight[i])
+    for i in ((Rssi)):
+        for j in range(len(i)):
+            Rssi_result.append(round(i[j] * Rssi_Weight[j],3))
     return Rssi_result
